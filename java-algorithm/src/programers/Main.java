abee3292bcce2005b4e439a08137c70faa4f68dc
won taek oh <dnjsxorchlrh@naver.com>
2023-12-14 13:05:23 +0900

    [프로그래머스 java] 두 큐 합 같게 만들기

--- java-algorithm/src/programers/Main.java
+++ java-algorithm/src/programers/Main.java
@@ -2,7 +2,6 @@ package programers;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class Main {
 
@@ -14,39 +13,16 @@ public class Main {
     }
 
     public static int solution(int[] queue1, int[] queue2) {
-        long[] lQueue1 = new long[queue1.length];
-        long[] lQueue2 = new long[queue2.length];
-
-        for (int i = 0 ; i < lQueue1.length; i ++) {
-            lQueue1[i] = queue1[i];
-            lQueue2[i] = queue2[i];
-        }
-
         int startIndex = 0;
         int endIndex = queue1.length - 1;
 
-        long queue1Sum = LongStream.of(lQueue1).sum();
-        long queue2Sum = LongStream.of(lQueue2).sum();
+        long queue1Sum = IntStream.of(queue1).sum();
+        long queue2Sum = IntStream.of(queue2).sum();
         long goalSum = (queue1Sum + queue2Sum) / 2;
-        long sum;
         int count = 0;
 
-        if ((queue1Sum + queue2Sum) % 2 != 0) {
-            return -1;
-        }
-
-        long[] array;
-        if (queue1Sum < queue2Sum) {
-            long[] tempArray = LongStream.concat(Arrays.stream(lQueue1), Arrays.stream(lQueue2)).toArray();
-            array = LongStream.concat(Arrays.stream(tempArray), Arrays.stream(lQueue1)).toArray();
-            sum = queue1Sum;
-        } else if (queue1Sum > queue2Sum) {
-            long[] tempArray = LongStream.concat(Arrays.stream(lQueue2), Arrays.stream(lQueue1)).toArray();
-            array = LongStream.concat(Arrays.stream(tempArray), Arrays.stream(lQueue2)).toArray();
-            sum = queue2Sum;
-        } else {
-            return 0;
-        }
+        int[] array = IntStream.concat(Arrays.stream(queue1), Arrays.stream(queue2)).toArray();
+        long sum = queue1Sum;
 
         while(startIndex <= endIndex) {
             if (sum < goalSum) {
