6d1fd6159d65b7071ac09b2ab70de0b0fc3ae92f
won taek oh <wontaekoh@hansol.com>
2023-12-18 12:55:11 +0900

    [백준 java] 1010

--- java-algorithm/src/baekjoon/problem1010_다리놓기/Main.java
+++ java-algorithm/src/baekjoon/problem1010_다리놓기/Main.java
@@ -1,7 +1,31 @@
 package baekjoon.problem1010_다리놓기;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/**
+ * 백준 1010
+ * https://www.acmicpc.net/problem/1010
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0 ; i < n ; i++ ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.printf("%.0f%n", answer(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+    }
+
+    public static double answer(int eastCount, int westCount) {
+        double answer = 1;
+
+        for (int i = 0; i < eastCount ; i++ ) {
+            answer *= (double) (westCount - i) / (i + 1);
+        }
+
+        return answer;
     }
 }
