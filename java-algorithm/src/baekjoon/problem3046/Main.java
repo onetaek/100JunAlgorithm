0834f951ecd99b855a30065e0bc93757202b8079
won taek oh <dnjsxorchlrh@naver.com>
2024-01-03 20:31:12 +0900

    [백준 java] 3046

---
+++ java-algorithm/src/baekjoon/problem3046/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.problem3046;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println((b*2)-a);
+
+    }
+}
