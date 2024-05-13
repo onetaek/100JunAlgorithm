5a147892b3a476e7f1f047cd6034bc0d0b1daf58
won taek oh <dnjsxorchlrh@naver.com>
2024-05-14 00:26:41 +0900

    [백준 python] 117773(제로)
    - 자료구조, 스택

---
+++ python-algorithm/백준알고리즘/솔브드/실버/4단계/10773(제로).py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/10773
+
+import sys
+
+sys.stdin = open("input.txt", "r")
+N = int(input())
+numbers = [int(input()) for _ in range(N)]
+queue = []
+"""
+<코딩하기전 로직 정리>
+- for문을 돌면서 numbers의 각 원소를 더한다.
+- 0을 만나면 이전 index의 값을 빼주면 될 것 같다.
+- 연속으로 0을 만날 경우 위 조건으로 처리못함
+- 스택을 사용하는게 적절하겠구만
+"""
+for i in range(len(numbers)):
+    number = numbers[i]
+    if (number == 0):
+        queue.pop()
+    else:
+        queue.append(number)
+
+print(sum(queue))
+
